5ef2573d3fa0a2aed679bf1ed6cf93813b18b37c
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2021-02-22 21:16:54 +0200

    Enter variables

---
+++ js_upgrade-robot.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const robot = {
+  chipVer: 9,
+  serialNo: 413,
+  wheels: 2,
+};
+
+const parts = [
+  { gravitationSensor: true },
+  { wheels: 6 },
+  { chipVer: 16 },
+  { ram: 1024 },
+  { displays: 2 },
+];
